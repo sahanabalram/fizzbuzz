e01927b95ba4d67a447e5aea54f0c83ed25d099a
Sahana Balram <sahana2008balram@gmail.com>
2018-01-12 15:14:56 -0800

    FizzBuzz

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-function myFizzBuzz(start, end, multipleOfFirstNumber, multipleOfSecondNumber) {
-    for (var i = start; i <= end; i++) {
-      
-      // if a number is a multiple of 3 print out fizz 
-      if(i % multipleOfFirstNumber === 0 && i % multipleOfSecondNumber === 0) {
-        // do this code inside of here
-        console.log("fizzbuzz");
-      } else if (i % multipleOfFirstNumber === 0) {
-        console.log("fizz");
-      } else if (i % multipleOfSecondNumber === 0) {
-        console.log("buzz");
-      } else {
-        console.log(i);
-      }
-    } 
+for(var i = 0; i <= 10; i++){
+  if(i%15 === 0) {
+    console.log("FizzBuzz");
+  } else if(i % 3 === 0){
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
   }
-
-  myFizzBuzz(50, 400, 10, 13);
-
-  myFizzBuzz(1, 100, 3, 5);
+}
